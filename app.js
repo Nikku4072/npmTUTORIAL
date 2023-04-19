d38e59f4015b91429b2486e7977fb42352eefa00
nikku <nikku4072192@gmail.com>
2023-04-20 01:26:17 +0530

    third commit

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
-
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
-app.use((req,res,next)=>{
-    console.log('in the middleware!');
-    next();//alllows the request to continue to next middleware.
+// Use body-parser middleware to parse incoming requests
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Display a form to add a product
+app.get('/add-product', (req, res) => {
+  res.send(`
+    <form action="/product" method="POST">
+      <label for="title">Title:</label>
+      <input type="text" id="title" name="title">
+      <br>
+      <label for="size">Size:</label>
+      <input type="text" id="size" name="size">
+      <br>
+
+      <button type="submit">Add Product</button>
+    </form>
+  `);
 });
 
-app.use((req,res,next)=>{
-    console.log('in another middleware!');
-    res.send('<h1>HELLO FROM EXPRESS</h1>');
+// Log the submitted data to the console
+app.post('/product', (req, res) => {
+    const { title, size } = req.body;
+    console.log(`Title: ${title}, Size: ${size}`);
+    res.send('Product added successfully!');
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
